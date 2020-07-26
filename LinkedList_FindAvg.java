3699159374931c20b61ef927b9d3ff4183c8b29d
Anurag-6868 <68323200+Anurag-6868@users.noreply.github.com>
2020-07-26 23:04:36 +0530

    Add files via upload

---
+++ LinkedList_FindAvg.java
@@ -0,0 +1,71 @@
+package fourth;
+import java.util.Scanner;
+class Node<T> {
+
+	
+   T data;
+   Node<T> next;
+   
+   Node(T data)
+   {
+	   this.data=data;
+	   next=null;
+   }
+	
+
+}
+public class LinkedList_FindAvg {
+
+	
+	public static Node<Integer> takeInput()
+	{
+		Node<Integer> head = null,tail=null;
+		Scanner s = new Scanner(System.in);
+		int data = s.nextInt();
+		while(data!=-1)
+		{
+			Node<Integer> newNode = new Node<Integer>(data);
+			
+			if(head==null)
+			{
+				head= newNode;
+				tail = newNode;
+			}
+			else
+			{	
+				tail.next= newNode; 
+				tail= newNode;
+			} 
+			
+			data = s.nextInt();
+		}
+		return head;
+	}
+	
+	public static void CalculateAvg(Node<Integer> head)
+	{
+		
+		int s=0,c=0;
+		
+		while(head!=null)
+		{
+			s=s+head.data;
+			head=head.next;
+			c++;
+		}
+		
+		int avg=(int)s/c;
+		
+		System.out.println(avg);
+		
+	}
+	
+	public static void main(String[] args) {
+		
+
+		Node<Integer> head = takeInput();
+		
+		CalculateAvg(head);
+	}
+
+}

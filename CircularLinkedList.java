fcd9b5c0cc0a052e9b550c12e42c2ecc6f4dfbe7
Anurag-6868 <68323200+Anurag-6868@users.noreply.github.com>
2020-08-07 22:41:21 +0530

    Add files via upload

---
+++ CircularLinkedList.java
@@ -0,0 +1,68 @@
+package fifth;
+
+public class CircularLinkedList {
+	  public class Node{  
+          int data;  
+          Node next;  
+          public Node(int data) {  
+              this.data = data;  
+          }  
+      }    
+      public Node head = null;  
+      public Node tail = null;  
+
+      //This function will add the new node at the end of the list.  
+      public void addAtStart(int data){  
+          Node newNode = new Node(data);  
+          //Checks if the list is empty.  
+          if(head == null) {  
+               //If list is empty, both head and tail would point to new node.  
+              head = newNode;  
+              tail = newNode;  
+              newNode.next = head;  
+          }  
+          else {  
+              //Store data into temporary node  
+              Node temp = head;  
+              //New node will point to temp as next node  
+              newNode.next = temp;  
+              //New node will be the head node  
+              head = newNode;  
+              //Since, it is circular linked list tail will point to head.  
+              tail.next = head;  
+          }  
+      }  
+ 
+      public void display() {  
+          Node current = head;  
+          if(head == null) {  
+              System.out.println("List is empty");  
+          }  
+          else {  
+              System.out.println("Adding nodes to the start of the list: ");  
+               do{  
+                  //Prints each node by incrementing pointer until current!=head rahe ga.  
+                  System.out.print(" "+ current.data);  
+                  current = current.next;  
+              }while(current != head);  
+              System.out.println();  
+          }  
+      }  
+
+      public static void main(String[] args) {  
+          CircularLinkedList cl = new CircularLinkedList();  
+
+           
+          cl.addAtStart(1);  
+          cl.display();  
+           
+          cl.addAtStart(2);  
+          cl.display();  
+         
+          cl.addAtStart(3);  
+          cl.display();  
+           
+          cl.addAtStart(4);  
+          cl.display();  
+      }
+}

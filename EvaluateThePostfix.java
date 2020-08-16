6a1f35a7754b3436735caf96709841fb7d183005
Anurag-6868 <68323200+Anurag-6868@users.noreply.github.com>
2020-08-16 22:36:54 +0530

    Add files via upload

---
+++ EvaluateThePostfix.java
@@ -0,0 +1,52 @@
+package fifth;
+import java.util.*;
+public class EvaluateThePostfix {
+	static int evaluatePostfix(String exp) 
+    { 
+       
+        Stack<Integer> stack=new Stack<>(); 
+          
+       
+        for(int i=0;i<exp.length();i++) 
+        { 
+            char c=exp.charAt(i); 
+              
+            if(Character.isDigit(c)) 
+            stack.push(c - '0'); 
+               
+            else
+            { 
+                int val1 = stack.pop(); 
+                int val2 = stack.pop(); 
+                  
+                switch(c) 
+                { 
+                    case '+': 
+                    stack.push(val2+val1); 
+                    break; 
+                      
+                    case '-': 
+                    stack.push(val2- val1); 
+                    break; 
+                      
+                    case '/': 
+                    stack.push(val2/val1); 
+                    break; 
+                      
+                    case '*': 
+                    stack.push(val2*val1); 
+                    break; 
+              } 
+            } 
+        } 
+        return stack.pop();     
+    } 
+      
+    public static void main(String[] args)  
+    { 
+    	Scanner s = new Scanner(System.in);
+    	String exp = s.next();
+//        String exp="231*+9-"; 
+        System.out.println("postfix evaluation: "+evaluatePostfix(exp)); 
+    }
+}
